bd3908b49266520a498fe647d2e16714e34210bd
David Fischer <d_fi@gmx.de>
2020-11-29 23:42:48 +0100

    add: default paths

--- sync_kobo/__init__.py
+++ sync_kobo/__init__.py
@@ -1,1 +1,12 @@
 """__init__ file."""
+
+from appdirs import AppDirs
+
+PROJECT_NAME = "sync-kobo"
+AUTHOR = "David Fischer"
+
+dirs = AppDirs(appname=PROJECT_NAME, appauthor=AUTHOR)
+
+
+CONFIG_DIR = dirs.user_config_dir
+APP_DIR = dirs.user_data_dir
